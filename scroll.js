f1ced977888141571e5427264f542692e38187a4
seba.developer <cmoralesaroca@gmail.com>
2023-02-21 23:51:42 -0300

    Falta el menu responsivo

---
+++ scroll.js
@@ -0,0 +1,6 @@
+const menuBeach = document.getElementById ("menuBeach") ;
+const menuDesert = document.getElementById ("menuDesert") ;
+const menuNature = document.getElementById ("menuNature") ;
+const menuOutdoor = document.getElementById ("menuOutdoor") ;
+
+
